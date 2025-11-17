65c7968c27afe657647eb9e110ab6580c301c310
Dariel-dev-cloud <ortegasorzanodariel@gmail.com>
2025-11-17 17:39:10 -0500

    Add DailyForecast component and update App to include it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { getWeather } from "./api"
 import Card from "./components/cards/Card"
+import DailyForecat from "./components/cards/DailyForecat"
 
 
 function App() {
@@ -13,9 +14,9 @@ function App() {
   return (
     <>
     <div className="flex flex-col gap-8" >
-    <Card title="Tiempo Actual" >{JSON.stringify(data?.current).slice(0, 100)}</Card>
-    <Card title="Pronóstico por hora (48 horas) " >{JSON.stringify(data?.hourly).slice(0, 100)}</Card>
-    <Card title="Pronóstico Diario" >{JSON.stringify(data?.daily).slice(0, 100)}</Card>
+    <Card title="Tiempo Actual" >{JSON.stringify(data?.current)?.slice(0, 100)}</Card>
+    <Card title="Pronóstico por hora (48 horas) " >{JSON.stringify(data?.hourly)?.slice(0, 100)}</Card>
+    <DailyForecat/>
     </div>
       </>
   ) 
---
+++ src/components/cards/DailyForecat.tsx
@@ -0,0 +1,30 @@
+
+import { useSuspenseQuery } from '@tanstack/react-query'
+import Card from './Card'
+import { getWeather } from '../../api'
+
+export default function DailyForecat() {
+     const {data} = useSuspenseQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({lat:10, lon:25})
+  })
+
+  return (
+    <Card title="Pronóstico Diario" childrenClassName='flex flex-col gap-4' >
+     
+      {data?.daily.map(day=>(
+        <div key={day.dt} className='flex justify-between'>
+          <p className='w-9' >{new Date(day.dt * 1000).toLocaleDateString(undefined,{
+            weekday:'short',
+          })}</p>
+          <img className='size-8'
+           src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} alt='tiempo icon' />
+          <p>{ Math.round(day.temp.day)}°F</p>
+          <p className='text-gray-500/75 ' >{ Math.round(day.temp.min)}°F</p>
+          <p className='text-gray-500/75 ' >{ Math.round(day.temp.max)}°F</p>
+        </div>
+      ))}
+
+      </Card>
+  )
+}
